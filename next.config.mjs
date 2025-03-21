3acd7a67aae6e54c55cdb011ffbcabbdf6bc31cd
Nguyễn Phan Vũ <npvu1510@gmail.com>
2025-03-21 20:53:18 +0700

    change GCS path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix:
-    'https://storage.googleapis.com/static.saoladigital.com/en-vocab-fe',
+    'https://storage.googleapis.com/static.saoladigital.com/en-vocab-fe-2',
 };
-
+// "static.saoladigital.com/en-vocab-fe/_next/static"
 export default nextConfig;
